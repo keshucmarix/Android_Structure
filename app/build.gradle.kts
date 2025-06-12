041226cc7f21004444d9d5efb9bf5d3bd01d47b7
keshu <kodedara@sentrilock.com>
2025-06-12 17:49:25 +0530

    54.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,7 +101,7 @@ android {
             property("sonar.projectKey", "Android-Architecture")
             property("sonar.projectName", "Android-Architecture")
             property("sonar.host.url", "http://203.109.113.153:9000")
-            property("sonar.login", System.getenv("SONAR_TOKEN"))
+            property("sonar.login", "sqp_5fdd6887be37a7fbc3ed677377a64e65fa85576b")
             property("sonar.sources", listOf("src/main/java")) // ✅ Must be a list, not a String!
             property("sonar.sourceEncoding", "UTF-8")
             property("sonar.java.binaries", listOf("build")) // Optional but helps with analysis
